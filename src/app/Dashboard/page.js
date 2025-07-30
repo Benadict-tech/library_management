d5151efd8c20d24114867b4c71a457af00c8f227
Benadict <benadictlouis1999@gmail.com>
2025-07-30 20:24:03 +0530

    feat:login auth

---
+++ src/app/Dashboard/page.js
@@ -0,0 +1,10 @@
+
+
+export default function Dashboard(){
+
+return(
+    <div>
+        <h1>DashBoard Inprocess......</h1>
+    </div>
+)
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,39 @@
+"use client";
 import Link from "next/link";
 import "../CustomCss/home.css";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const router = useRouter();
+
+  function login() {
+    if(!email||!password){
+      alert("Fill all the entries")
+      return""
+    }
+    const existingUser = JSON.parse(localStorage.getItem("users")) || [];
+
+    const matchedUser= existingUser.find((user)=>user.email===email&&user.password===password)
+    
+
+    if (!matchedUser){
+      alert(" Email or password mismatch  (or)  Create a account for logging in")
+      setEmail("")
+      setPassword("")
+      
+      return
+
+    }
+    
+    router.push("/Dashboard")
+  
+    
+    
+  }
+
   return (
     <div className="full-page">
       <div className="content-box">
@@ -11,14 +43,20 @@ export default function Home() {
           type="email"
           className="input-item"
           placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         ></input>
         <input
           id="password"
           type="password"
           className="input-item"
           placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         ></input>
-        <button className="btw-login">Login</button>
+        <button className="btw-login" onClick={login}>
+          Login
+        </button>
         <p className="forgot-password">forgot password?</p>
         <div className="sub-division">
           <p className="create-account">
